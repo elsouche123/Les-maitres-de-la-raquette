eae84669cedcd588204e935ccbee76cec1acc3b6
iPwaoo <qbouetel@iut.univ-paris8.fr>
2024-04-03 19:47:45 +0200

    Systeme du numero d'inscription

--- back/services/algorithmes/joueur/joueur_insertion.py
+++ back/services/algorithmes/joueur/joueur_insertion.py
@@ -1,7 +1,7 @@
 from back.services.connexion.DatabaseService import DatabaseService
 
 
-def insertion_joueur(genre: str, nom: str, prenom: str, age: int, courriel: str, telephone: str, adresse: str, codePostale: int, ville: str, pays: str, numeroInscription: str, licence: str, classement: int):
+def insertion_joueur(genre: str, nom: str, prenom: str, age: int, courriel: str, telephone: str, adresse: str, codePostale: int, ville: str, pays: str, licence: str, classement: int):
     db = DatabaseService()
     collection = db.get_collection("joueur")
     document = {
@@ -15,11 +15,17 @@ def insertion_joueur(genre: str, nom: str, prenom: str, age: int, courriel: str,
         "codePostale": codePostale,
         "ville": ville,
         "pays": pays,
-        "numeroInscription": numeroInscription,
         "licence": licence,
         "classement": classement,
     }
-    collection.insert_one(document)
+    result = collection.insert_one(document).inserted_id
+
+    # Extraction des 8 derniers caractères de l'ObjectId
+    objectid_last_8_chars = str(result)[-8:]
+    print(objectid_last_8_chars)
+    filter = {"_id": result}
+    update = {"$set": {"numeroInscription": objectid_last_8_chars}}
+    collection.update_one(filter, update)
     db.seDeconnecter()
 
 
