d77aa5801e4a319015d314c2c1f26bfcff1522be
iPwaoo <qbouetel@iut.univ-paris8.fr>
2024-04-04 22:42:32 +0200

    Debug

--- back/api/app.py
+++ back/api/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
+
 from tournois_routes import tournois_bp
 from joueur_routes import joueur_bp
 from equipe_route import equipe_bp
 from calcule_routes import calcule_bp
 
 app = Flask(__name__)
+cors = CORS(app)
 
 app.register_blueprint(joueur_bp, url_prefix='/api/joueur')
 app.register_blueprint(tournois_bp, url_prefix='/api/tournois')
