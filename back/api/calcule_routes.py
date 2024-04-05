770017dbb783d1afaa013630c554a4b3c7394e2d
elsa hadjadj <ehadjadj@iut.univ-paris8.fr>
2024-04-05 11:58:56 +0200

    calcule temps tournois

--- back/api/calcule_routes.py
+++ back/api/calcule_routes.py
@@ -15,6 +15,6 @@ def calc_max_participant(heure, nb_table):
 @calcule_bp.route('/mele_general/max temps/<string:max_participant>/<int:nb_table>', methods=['GET'])
 def calc_max_temps(max_participant, nb_table):
     max_temps = {
-        "maxTemps": mg.calcul_temps_max(max_participant,nb_table)
+        "maxTemps": mg.calcul_temps_max(max_participant, nb_table)
     }
     return jsonify(max_temps)
